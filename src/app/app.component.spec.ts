23f4333d0a42a3897487d9f80422a0d0031aa872
Adria Huertas <adriahc@gmail.com>
2023-11-08 16:28:01 +0100

    Introduced TestBed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Angular Unit Testing');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'Angular Unit Testing app is running!'
-    );
-  });
 });
--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,21 +1,34 @@
 import { Post } from 'src/app/models/Post';
 import { PostComponent } from './post.component';
 import { first } from 'rxjs/operators';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('Post component', () => {
+  let fixture: ComponentFixture<PostComponent>;
+  let comp: PostComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PostComponent],
+    });
+    const fixture = TestBed.createComponent(PostComponent);
+    comp = fixture.componentInstance;
+  });
+  it('should create post component using TestBed', () => {
+    expect(comp).toBeDefined();
+  });
   it('should raise an event when the delete post is clicked', () => {
-    const component = new PostComponent();
     const post: Post = {
       id: 1,
       title: 'Test Post',
       body: 'Test Body',
       userId: 5,
     };
-    component.post = post;
-    component.delete.pipe(first()).subscribe((p) => {
+    comp.post = post;
+    comp.delete.pipe(first()).subscribe((p) => {
       expect(p).toEqual(post);
     });
 
-    component.onDeletePost(new MouseEvent('click'));
+    comp.onDeletePost(new MouseEvent('click'));
   });
 });
